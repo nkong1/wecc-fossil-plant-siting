6199bafe2763f637648b3c651ee32521b37e2218
Nicholas Kong <kong_nicholas@berkeley.edu>
2025-09-01 12:03:17 -0700

    update reference plant specs

--- pre_processing/reference_plant_specs.py
+++ pre_processing/reference_plant_specs.py
@@ -1,25 +1,25 @@
 # Create a dictionary mapping each hydrogen production technology to its square reference plant footprint length (m)
 ref_footprint = {
-    "gas_smr": 332.6,
-    "gas_smr_ccs": 332.6,
-    "biogas_smr": 332.6,
-    "biogas_smr_ccs": 332.6,
-    "gas_atr_ccs": 332.6,
-    "biogas_atr_ccs": 332.6,
-    "coal_gasification": 576.0,
-    "coal_gasification_ccs": 576.0,
-    "biomass_gasification": 279.1,
+    "gas_smr": 354.5,
+    "gas_smr_ccs": 354.5,
+    "biogas_smr": 354.5,
+    "biogas_smr_ccs": 354.5,
+    "gas_atr_ccs": 354.5,
+    "biogas_atr_ccs": 354.5,
+    "coal_gasification": 614.0,
+    "coal_gasification_ccs": 614.0,
+    "biomass_gasification": 297.6,
 }
 
 # Create a dictionary mapping each hydrogen production technology to its reference plant capacity (tonnes/day)
 ref_capacity = {
-    "gas_smr": 132,
-    "gas_smr_ccs": 132,
-    "biogas_smr": 132,
-    "biogas_smr_ccs": 132,
-    "gas_atr_ccs": 180,
-    "biogas_atr_ccs": 180,
-    "coal_gasification": 180,
-    "coal_gasification_ccs": 180,
-    "biomass_gasification": 42,
+    "gas_smr": 150,
+    "gas_smr_ccs": 150,
+    "biogas_smr": 150,
+    "biogas_smr_ccs": 150,
+    "gas_atr_ccs": 205,
+    "biogas_atr_ccs": 205,
+    "coal_gasification": 205,
+    "coal_gasification_ccs": 205,
+    "biomass_gasification": 48,
 }
