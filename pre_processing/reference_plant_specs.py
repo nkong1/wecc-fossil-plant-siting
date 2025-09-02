ab35e5e62c8d38dac0fe28f45baec54b406c3445
Nicholas Kong <kong_nicholas@berkeley.edu>
2025-09-01 22:05:40 -0700

    standardize tech names

--- pre_processing/reference_plant_specs.py
+++ pre_processing/reference_plant_specs.py
@@ -2,24 +2,24 @@
 ref_footprint = {
     "gas_smr": 354.5,
     "gas_smr_ccs": 354.5,
-    "biogas_smr": 354.5,
-    "biogas_smr_ccs": 354.5,
+    "bio_smr": 354.5,
+    "bio_smr_ccs": 354.5,
     "gas_atr_ccs": 354.5,
-    "biogas_atr_ccs": 354.5,
-    "coal_gasification": 614.0,
-    "coal_gasification_ccs": 614.0,
-    "biomass_gasification": 297.6,
+    "bio_atr_ccs": 354.5,
+    "coal_gas": 614.0,
+    "coal_gas_ccs": 614.0,
+    "biomass_gas": 297.6,
 }
 
 # Create a dictionary mapping each hydrogen production technology to its reference plant capacity (tonnes/day)
 ref_capacity = {
     "gas_smr": 150,
     "gas_smr_ccs": 150,
-    "biogas_smr": 150,
-    "biogas_smr_ccs": 150,
+    "bio_smr": 150,
+    "bio_smr_ccs": 150,
     "gas_atr_ccs": 205,
-    "biogas_atr_ccs": 205,
-    "coal_gasification": 205,
-    "coal_gasification_ccs": 205,
-    "biomass_gasification": 48,
+    "bio_atr_ccs": 205,
+    "coal_gas": 205,
+    "coal_gas_ccs": 205,
+    "biomass_gas": 48,
 }
