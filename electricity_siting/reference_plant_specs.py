ea13967c962d49416bfeb50867999ae7043e9be1
Nicholas Kong <kong_nicholas@berkeley.edu>
2025-10-01 14:42:07 -0700

    added calculation for the potential capacity of each electricity generating technology in each load zone

--- electricity_siting/reference_plant_specs.py
+++ electricity_siting/reference_plant_specs.py
@@ -1,9 +1,8 @@
 
-# Create a dictionary mapping each hydrogen production technology to its reference plant capacity (tonnes/day)
+# Create a dictionary mapping each electricity generating technology to its reference nameplate capacity (MW)
 ref_capacity = {
     "gas_cc": 1009,
     "gas_cc_ccs": 943.5,
     "coal_igcc": 764.3,
     "coal_igcc_ccs": 707.7,
 }
-
