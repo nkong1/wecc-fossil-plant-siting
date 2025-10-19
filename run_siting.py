6ce608067ef46b9a70518e4a64818c6d7e40fb2b
Nicholas Kong <kong_nicholas@berkeley.edu>
2025-10-19 01:17:13 -0700

    add more detailed documentation

--- run_siting.py
+++ run_siting.py
@@ -2,8 +2,18 @@
 Use this file to run the combined siting for electricity and hydrogen plants. Be sure to have 
 the correct required input files in the user_inputs folder.
 
-Note: the formatting of all input files exactly match that of the examples, with the exception of 
-the hydrogen demand .gpkg file name (it can be anything, as long as it is a .gpkg file).
+Three files are required for hydrogen siting:
+1) h2_buildout.csv: contains the build-out (MW) of each H2 technology in every load zone
+2) h2_capacity_factors.csv: capacity factors of each H2 technology in every load zone
+3) A .gpkg file containing the hydrogen demand in the WECC at a 5x5km resolution. This 
+    should be takendirectly from the H2 demand model)
+
+One file is required for the electricity generator siting:
+1) gen_buildout.csv: contains the build-out (MW) of each generation technology in every load zone
+
+Please refer to the example file formatting when creating the inputs. The formatting of all input 
+files exactly match that of the examples, with the exception of the hydrogen demand .gpkg file name 
+(it can be named anything, as long as it is a .gpkg file).
 """
 import shutil
 from pathlib import Path
